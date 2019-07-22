a960691c1abd77749103b426fe1a8d36a30faeb0
Spr-Chan <im_cyc@163.com>
2019-07-22 15:42:46 +0800

    Site updated: 2019-07-22 15:42:45

--- js/tagcloud.js
+++ js/tagcloud.js
@@ -13,9 +13,9 @@ addLoadEvent(function() {
    try {
        TagCanvas.textFont = 'Trebuchet MS, Helvetica';
        TagCanvas.textColour = '#333';
-       TagCanvas.textHeight = 20;
+       TagCanvas.textHeight = 22;
        TagCanvas.outlineColour = '#E2E1D1';
-       TagCanvas.maxSpeed = 0.1;
+       TagCanvas.maxSpeed = 0.2;
        TagCanvas.outlineMethod = 'block';
        TagCanvas.minBrightness = 0.2;
        TagCanvas.depth = 0.92;
